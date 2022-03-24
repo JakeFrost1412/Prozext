078bcfad7ac3e5a7058de1cc161ff3575536f1ed
JakeFrost1412 <94905344+JakeFrost1412@users.noreply.github.com>
2022-03-24 16:25:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Treasury from './pages/Treasury';
+//import './App.css';
+//import Sidebar from './pages/Sidebar';
+
+function App() {
+    return (
+        <>
+        <Treasury />
+        </>
+    );
+}
+
+export default App;
